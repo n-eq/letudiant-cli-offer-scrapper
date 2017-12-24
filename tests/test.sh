1722bdbc7e3e0a8c99f0eb519b1f9ef44c3e2e06
Nabil Elqatib <nabilelqatib@gmail.com>
2017-12-24 18:37:04 +0100

    Update test script
    Check that, for each test vector (parse n pages), the number of
    resulting lines is correct, and that there are no duplicate lines

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,23 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-for i in `seq 1 5`; do
-	echo "python offer-scrapper/main.py --pages $i ..."
-	python offer-scrapper/main.py --pages "$i"
+WHITE='\033[1;37m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# test (multi)page parsing
+for i in $(seq 1 10); do
+	printf "${WHITE}python offer-scrapper/main.py --pages $i ..."
+	python offer-scrapper/main.py --pages "$i" >> tests/tmp_$$
+    # check that we have the correct numberof lines  (10 (offers/page) + 1) per page requested
+    # .. and check also that the lines are unique (no duplicates as in previous issues)
+    if [[ $(uniq tests/tmp_$$| wc -l| cut -d' ' -f1) -ne  $((i * 11)) ]]; then
+        printf "${RED} failed${NC}\n"
+        rm -f tests/tmp_$$
+        exit
+    else
+        printf "${GREEN} OK${NC}\n"
+    fi
+    rm -f tests/tmp_$$
 done
 
-for filter in C Paris Web; do
-	echo "python offer-scrapper/main.py --filter $filter ..."
-	python offer-scrapper/main.py --filter "$filter"
-done
-
-echo "Done, so far so good."
