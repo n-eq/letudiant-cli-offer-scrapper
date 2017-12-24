4f9d49b76b98730306621e4ea52ca9ae9bdd6290
Nabil Elqatib <nabilelqatib@gmail.com>
2017-12-24 18:57:04 +0100

    Fix syntax in setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name="letudiant-cli-offer-scrapper",
+        version='0.1',
+        py_modules=['main'],
+        entry_points={
+            'console_scripts': ['letudiant-cli-offer-scrapper = letudiant-cli-offer-scrapper.main:main'],
+            }
+)
+
