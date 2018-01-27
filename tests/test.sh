9a4f8edeecf1a53350d6fb1a26747b5655f331c9
Nabil Elqatib <nabilelqatib@gmail.com>
2018-01-27 10:29:33 +0100

    Create functions for each test

--- tests/test.sh
+++ tests/test.sh
@@ -5,19 +5,28 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# test (multi)page parsing
-for i in $(seq 1 10); do
-	printf "${WHITE}main.py --pages $i ..."
-	python offer-scrapper/main.py --pages "$i" >> tests/tmp_$$
-    # check that we have the correct numberof lines  (10 (offers/page) + 1) per page requested
-    # .. and check also that the lines are unique (no duplicates as in previous issues)
-    if [[ $(uniq tests/tmp_$$| wc -l| cut -d' ' -f1) -ne  $(((i * 11) - 3)) ]]; then
-        printf "${RED} failed${NC}\n"
+test_multi_pages(){
+    for i in $(seq 1 10); do
+        printf "${WHITE}main.py --pages $i ..."
+        python offer-scrapper/main.py --pages "$i" >> tests/tmp_$$
+        # check that we have the correct numberof lines  (10 (offers/page) + 1) per page requested
+        # .. and check also that the lines are unique (no duplicates as in previous issues)
+        if [[ $(uniq tests/tmp_$$| wc -l| cut -d' ' -f1) -ne  $(((i * 11) - 3)) ]]; then
+            printf "${RED} failed${NC}\n"
+            rm -f tests/tmp_$$
+            exit
+        else
+            printf "${GREEN} OK${NC}\n"
+        fi
         rm -f tests/tmp_$$
-        exit
-    else
-        printf "${GREEN} OK${NC}\n"
-    fi
-    rm -f tests/tmp_$$
-done
+    done
+}
+
+# TODO: e.g., use: 'echo -e "1\rq\r"| ./offer-scrapper/main.py -i' to trigger the first offer
+test_interactive(){
+    echo "Not yet inmplemented"
+}
+
+test_multi_pages
+test_interactive
 
